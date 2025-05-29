fe7b9c8c2c59f725820bd8dbeb9dac04caf7d52d
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-05-28 23:04:13 -0300

    Recurso para localizar um usuário pelo id

--- src/main/java/com/apirest/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/apirest/demo_park_api/service/UsuarioService.java
@@ -1,11 +1,11 @@
 package com.apirest.demo_park_api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.apirest.demo_park_api.entity.Usuario;
 import com.apirest.demo_park_api.repository.UsuarioRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor // injeção de dependencia via construtor / lombok
@@ -17,6 +17,12 @@ public class UsuarioService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado."));
     }
 
 }
--- src/main/java/com/apirest/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/apirest/demo_park_api/web/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package com.apirest.demo_park_api.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,17 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    // CRIAR USUARIO
     @PostMapping
     public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
 }
