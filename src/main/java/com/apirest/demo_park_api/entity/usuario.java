f5154764af0acdd6b9d7e2c4155dfb2b45d37f68
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-05-28 21:25:41 -0300

    Classe de Entidade Usuario

---
+++ src/main/java/com/apirest/demo_park_api/entity/usuario.java
@@ -0,0 +1,80 @@
+package com.apirest.demo_park_api.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "usuarios")
+public class usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @Column(name = "criador_por")
+    private String criadorPor;
+
+    @Column(name = "modificado_por")
+    private String ModificadoPor;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENTE
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        usuario other = (usuario) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
