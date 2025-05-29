042a3db4eb0e9aaee9f913a2337b1f578354b62e
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-05-28 22:35:47 -0300

    Requisição via Postman para criar um usuário

--- src/main/java/com/apirest/demo_park_api/entity/usuario.java
+++ src/main/java/com/apirest/demo_park_api/entity/usuario.java
@@ -3,13 +3,13 @@ package com.apirest.demo_park_api.entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
