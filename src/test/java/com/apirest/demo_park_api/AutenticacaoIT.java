ec7e20dfe126dcdb5da3e63de315bab22b797d59
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-06-08 20:10:01 -0300

    Teste de autenticação com status code 200

---
+++ src/test/java/com/apirest/demo_park_api/AutenticacaoIT.java
@@ -0,0 +1,36 @@
+package com.apirest.demo_park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.apirest.demo_park_api.jwt.JwtToken;
+import com.apirest.demo_park_api.web.dto.UsuarioLoginDto;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class AutenticacaoIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void autenticar_ComCrendenciasValidas_RetornarTokenComStatus200() {
+        JwtToken responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("testeApi@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+
+    }
+}
