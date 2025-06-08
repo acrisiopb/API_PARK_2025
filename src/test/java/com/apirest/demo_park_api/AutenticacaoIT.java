8fc6fe03b7498dddf9ff21dab47a8e89bea0ed82
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2025-06-08 20:14:29 -0300

    Teste de autenticação com status code 400

--- src/test/java/com/apirest/demo_park_api/AutenticacaoIT.java
+++ src/test/java/com/apirest/demo_park_api/AutenticacaoIT.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.apirest.demo_park_api.jwt.JwtToken;
 import com.apirest.demo_park_api.web.dto.UsuarioLoginDto;
+import com.apirest.demo_park_api.web.exception.ErrorMessage;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -33,4 +34,35 @@ public class AutenticacaoIT {
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
 
     }
+
+    @Test
+    public void autenticar_ComCrendenciasInvalidas_RetornarErrorMessageStatus400() {
+        ErrorMessage errorMessage = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("invalido@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(errorMessage).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(errorMessage.getStatus()).isEqualTo(400);
+
+        errorMessage = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("testeApi@gmail.com", "123453"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(errorMessage).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(errorMessage.getStatus()).isEqualTo(400);
+
+    }
+
 }
