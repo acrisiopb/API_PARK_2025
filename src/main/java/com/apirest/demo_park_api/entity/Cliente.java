d103fd6affa3de7d0cedbd14b5bf61abf4aca639
Acrísio_Cruz <acrisiopb@gmail.com>
2025-06-09 08:16:16 -0300

    Criando e mapeando a entidade Cliente

---
+++ src/main/java/com/apirest/demo_park_api/entity/Cliente.java
@@ -0,0 +1,83 @@
+package com.apirest.demo_park_api.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "clientes")
+@EntityListeners(AuditingEntityListener.class)
+public class Cliente implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "cpf", nullable =  false, unique = true, length = 11)
+    private String cpf;
+    
+    //Relação de um para um
+    @OneToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuaurio;
+    
+    //Auditoria
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @CreatedBy
+    @Column(name = "criador_por")
+    private String criadorPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String ModificadoPor;
+
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+   
+
+    
+
+}
