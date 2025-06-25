825601ea9e1603610d58bf1aa7705fa8a0fb3d8a
Acrísio_Cruz <acrisiopb@gmail.com>
2025-06-25 08:15:46 -0300

    Adicionando o recurso no controller para consumir o relatório por cpf

--- src/main/java/com/apirest/demo_park_api/web/controller/EstacionamentoController.java
+++ src/main/java/com/apirest/demo_park_api/web/controller/EstacionamentoController.java
@@ -1,11 +1,13 @@
 package com.apirest.demo_park_api.web.controller;
 
+import java.io.IOException;
 import java.net.URI;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,8 +23,10 @@ import org.springframework.data.domain.Page;
 import com.apirest.demo_park_api.entity.ClienteVaga;
 import com.apirest.demo_park_api.jwt.JwtUserDetails;
 import com.apirest.demo_park_api.repository.projection.ClienteVagaProjection;
+import com.apirest.demo_park_api.service.ClienteService;
 import com.apirest.demo_park_api.service.ClienteVagaService;
 import com.apirest.demo_park_api.service.EstacionamentoService;
+import com.apirest.demo_park_api.service.JasperService;
 import com.apirest.demo_park_api.web.dto.EstacionamentoCreateDto;
 import com.apirest.demo_park_api.web.dto.EstacionamentoResponseDto;
 import com.apirest.demo_park_api.web.dto.PageableDTO;
@@ -38,6 +42,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
@@ -51,6 +56,8 @@ public class EstacionamentoController {
 
         private final EstacionamentoService estacionamentoService;
         private final ClienteVagaService clienteVagaService;
+        private final ClienteService clienteService;
+        private final JasperService jasperService;
 
         @Operation(summary = "Operação de check-in", description = "Recurso para dar entrada de um veículo no estacionamento. "
                         +
@@ -61,10 +68,8 @@ public class EstacionamentoController {
                                                         "- Nenhuma vaga livre foi localizada;", content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
                                         @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos", content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
                                         @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE", content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
-                        })
-        @PostMapping("/check-in")
-        @PreAuthorize("hasRole('ADMIN')")
-        public ResponseEntity<EstacionamentoResponseDto> checkin(@RequestBody @Valid EstacionamentoCreateDto dto) {
+                        }) @PostMapping("/check-in") @PreAuthorize("hasRole('ADMIN')") public ResponseEntity<EstacionamentoResponseDto> checkin(
+                                        @RequestBody @Valid EstacionamentoCreateDto dto) {
                 ClienteVaga clienteVaga = ClienteVagaMapper.toClienteVaga(dto);
                 estacionamentoService.checkIn(clienteVaga);
                 EstacionamentoResponseDto responseDto = ClienteVagaMapper.toDto(clienteVaga);
@@ -114,14 +119,11 @@ public class EstacionamentoController {
 
                         @Parameter(in = PATH, name = "cpf", description = "Número do CPF referente ao cliente a ser consultado.", required = true),
 
-                        @Parameter(in = QUERY, name = "page", description = "Representa a página retornada.", 
-                        content = @Content(schema = @Schema(type = "integer", defaultValue = "0"))),
+                        @Parameter(in = QUERY, name = "page", description = "Representa a página retornada.", content = @Content(schema = @Schema(type = "integer", defaultValue = "0"))),
 
-                        @Parameter(in = QUERY, name = "size", description = "Representa o total de elemento por página.", 
-                        content = @Content(schema = @Schema(type = "integer", defaultValue = "5"))),
+                        @Parameter(in = QUERY, name = "size", description = "Representa o total de elemento por página.", content = @Content(schema = @Schema(type = "integer", defaultValue = "5"))),
 
-                        @Parameter(in = QUERY, name = "sort", description = "Campo padrao de ordenação 'dataEntrada,asc'.", 
-                        array = @ArraySchema(schema = @Schema(type = "String", defaultValue = "dataEntrada,asc")), hidden = true)
+                        @Parameter(in = QUERY, name = "sort", description = "Campo padrao de ordenação 'dataEntrada,asc'.", array = @ArraySchema(schema = @Schema(type = "String", defaultValue = "dataEntrada,asc")), hidden = true)
 
         }, responses = {
                         @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso", content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = EstacionamentoResponseDto.class))),
@@ -138,11 +140,29 @@ public class EstacionamentoController {
 
         @GetMapping
         @PreAuthorize("hasRole('CLIENTE')")
-        public ResponseEntity<PageableDTO> getAllEstacionamentosdoCliente(@AuthenticationPrincipal JwtUserDetails user ,
+        public ResponseEntity<PageableDTO> getAllEstacionamentosdoCliente(@AuthenticationPrincipal JwtUserDetails user,
                         @Parameter(hidden = true) @PageableDefault(size = 5, sort = "dataEntrada", direction = Sort.Direction.ASC) Pageable pageable) {
-                Page<ClienteVagaProjection> projection = clienteVagaService.buscarTodosPorUsuarioId(user.getId(), pageable);
+                Page<ClienteVagaProjection> projection = clienteVagaService.buscarTodosPorUsuarioId(user.getId(),
+                                pageable);
                 PageableDTO dto = PageableMapper.toDto(projection);
                 return ResponseEntity.ok(dto);
+        }
+
+        @GetMapping("/relatorio")
+        @PreAuthorize("hasRole('CLIENTE')")
+        public ResponseEntity<Void> getRelatorio(HttpServletResponse response, @AuthenticationPrincipal JwtUserDetails user ) throws IOException{
+                
+                String cpf = clienteService.buscarPorUsuarioId(user.getId()).getCpf();
+                jasperService.addParams("CPF", cpf);
+               
+                byte[] bytes = jasperService.geraPDF();
+                
+                response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+                response.setHeader("Content-disposition", "inline; filename=" + System.currentTimeMillis() + ".pdf");
+                response.getOutputStream().write(bytes);
+
+                return ResponseEntity.ok().build();
+                
         }
 
 }
